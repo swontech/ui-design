8fa571addc625cb01a4000c5c6e825fd0330fec8
황어진 <lawliet9712@gmail.com>
2022-04-28 13:32:42 +0900

    react test

---
+++ mobilePage/lib/react/Component0.js
@@ -0,0 +1,231 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image
+} from 'react-native';
+
+export default () => {
+  return (
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "paddingStart": 19,
+        "paddingTop": 37,
+        "flex": 1
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start"
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start"
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start"
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start"
+      }
+    } >
+    <Image style = {
+      {
+        "width": 30,
+        "height": 30
+      }
+    }
+    source = {
+      {
+        /* add your source here */ }
+    }
+    />
+    </View>
+    <View style = {
+      {
+        "flexDirection": "row",
+        "alignItems": "flex-start",
+        "marginStart": 14,
+        "marginTop": 32
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginTop": 22,
+        "width": 127,
+        "height": 19,
+        "backgroundColor": "rgba(252, 242, 0, 244)"
+      }
+    }
+    />
+    <Text style = {
+      {
+        "fontFamily": "Apple SD Gothic Neo",
+        "fontWeight": "bold",
+        "fontSize": 36,
+        "color": "rgba(29, 29, 29, 255)",
+        "marginStart": -126.39
+      }
+    } > 접근권한 안내 </Text>
+    </View>
+    </View>
+    <Text style = {
+      {
+        "fontFamily": "Apple SD Gothic Neo",
+        "fontWeight": "400",
+        "fontSize": 13,
+        "letterSpacing": 0.9,
+        "color": "rgba(112, 112, 112, 255)",
+        "marginStart": 19,
+        "marginTop": 6
+      }
+    } > 안전한 서비스 이용을 위해 아래의 권한을 허용해 주세요 </Text>
+    </View>
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": 16,
+        "marginTop": 61
+      }
+    } >
+    <Image style = {
+      {
+        "width": 33,
+        "height": 33
+      }
+    }
+    source = {
+      {
+        /* add your source here */ }
+    }
+    />
+    </View>
+    </View>
+    <View style = {
+      {
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+      }
+    } > <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": 16,
+        "marginTop": 98
+      }
+    } >
+    <Image style = {
+      {
+        "width": 33,
+        "height": 33
+      }
+    }
+    source = {
+      {
+        /* add your source here */ }
+    }
+    />
+    </View>
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": 22,
+        "marginTop": 98.29
+      }
+    } >
+    <Text style = {
+      {
+        "fontFamily": "Apple SD Gothic Neo",
+        "fontWeight": "400",
+        "fontSize": 20,
+        "letterSpacing": 0.9,
+        "color": "rgba(112, 112, 112, 255)"
+      }
+    } > 파일(선택) </Text>
+    <Text style = {
+      {
+        "fontFamily": "Apple SD Gothic Neo",
+        "fontWeight": "100",
+        "fontSize": 10,
+        "color": "rgba(112, 112, 112, 255)",
+        "marginTop": 2
+      }
+    } > 프로필 사진 설정시 이미지 파일첨부 용도 </Text>
+    </View>
+    </View>
+    <View style = {
+      {
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+      }
+    } > <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": 16,
+        "marginTop": 31.71
+      }
+    } >
+    <Image style = {
+      {
+        "width": 33,
+        "height": 33
+      }
+    }
+    source = {
+      {
+        /* add your source here */ }
+    }
+    />
+    </View>
+    <Text style = {
+      {
+        "fontFamily": "Apple SD Gothic Neo",
+        "fontWeight": "100",
+        "fontSize": 11,
+        "letterSpacing": -0.8,
+        "color": "rgba(112, 112, 112, 255)",
+        "marginStart": 22,
+        "marginTop": 34.71
+      }
+    } > 선택적 접근 권한은 기능 사용시 허용이 필요하며,
+    비허용시에도 해당 기능 외 서비스 이용이 가능합니다. </Text>
+    </View>
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "paddingStart": 113.9,
+        "paddingTop": 16,
+        "marginStart": 11,
+        "marginTop": 53,
+        "width": 300,
+        "height": 81,
+        "borderRadius": 40.5,
+        "backgroundColor": "rgba(0, 135, 141, 255)"
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start"
+      }
+    } >
+    <Text style = {
+      {
+        "fontFamily": "Apple SD Gothic Neo",
+        "fontWeight": "bold",
+        "fontSize": 41,
+        "letterSpacing": 0.9,
+        "color": "rgba(255, 255, 255, 255)"
+      }
+    } > 확인 </Text>
+    </View>
+    </View>
+    </View>
+
+  );
+};
