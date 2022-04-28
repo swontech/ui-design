1fce595dc4cc30147daa643eacd483c1f5936d6b
황어진 <lawliet9712@gmail.com>
2022-04-28 09:33:42 +0900

    회원등록 변경

--- mobilePage/lib/pageFolder/signUp.dart
+++ mobilePage/lib/pageFolder/signUp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:adobe_xd/pinned.dart';
-import './iconnext.dart';
 import './iconclose.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -15,7 +14,7 @@ class signUp extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Pinned.fromPins(
-            Pin(startFraction: 0.0917, endFraction: 0.5278),
+            Pin(startFraction: 0.0917, endFraction: 0.6278),
             Pin(startFraction: 0.1547, endFraction: 0.7781),
             child: Stack(
               children: <Widget>[
@@ -27,10 +26,10 @@ class signUp extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.fromLTRB(1.0, 0.0, 2.0, 0.0),
+                  padding: EdgeInsets.fromLTRB(1.0, 0.0, 7.0, 0.0),
                   child: SizedBox.expand(
                       child: Text(
-                    '회원 등록',
+                    '로그인',
                     style: TextStyle(
                       fontFamily: 'Apple SD Gothic Neo',
                       fontSize: 36,
@@ -48,7 +47,7 @@ class signUp extends StatelessWidget {
           ),
           Pinned.fromPins(
             Pin(start: 37.0, end: 36.0),
-            Pin(startFraction: 0.3203, endFraction: 0.5891),
+            Pin(startFraction: 0.3656, endFraction: 0.5438),
             child: Stack(
               children: <Widget>[
                 Pinned.fromPins(
@@ -87,46 +86,7 @@ class signUp extends StatelessWidget {
           ),
           Pinned.fromPins(
             Pin(start: 37.0, end: 36.0),
-            Pin(startFraction: 0.5891, endFraction: 0.3203),
-            child: Stack(
-              children: <Widget>[
-                Pinned.fromPins(
-                  Pin(startFraction: 0.0, endFraction: 0.7596),
-                  Pin(startFraction: 0.0, endFraction: 0.5862),
-                  child: Text(
-                    '비밀번호',
-                    style: TextStyle(
-                      fontFamily: 'Apple SD Gothic Neo',
-                      fontSize: 20,
-                      color: const Color(0xff1d1d1d),
-                      fontWeight: FontWeight.w600,
-                    ),
-                    softWrap: false,
-                  ),
-                ),
-                Pinned.fromPins(
-                  Pin(start: 0.0, end: 0.0),
-                  Pin(size: 0.0, end: 0.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, -1.0),
-                        child: SizedBox.expand(
-                            child: SvgPicture.string(
-                          _svg_w5t6nb,
-                          allowDrawingOutsideViewBox: true,
-                          fit: BoxFit.fill,
-                        )),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Pinned.fromPins(
-            Pin(start: 37.0, end: 36.0),
-            Pin(startFraction: 0.4547, endFraction: 0.4547),
+            Pin(startFraction: 0.5, endFraction: 0.4094),
             child: Stack(
               children: <Widget>[
                 Pinned.fromPins(
@@ -148,14 +108,14 @@ class signUp extends StatelessWidget {
                   Pin(size: 0.0, end: 0.0),
                   child: Stack(
                     children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, -1.0),
-                        child: SizedBox.expand(
-                            child: SvgPicture.string(
+                      Pinned.fromPins(
+                        Pin(startFraction: 0.0, endFraction: 0.0),
+                        Pin(startFraction: NaN, endFraction: -Infinity),
+                        child: SvgPicture.string(
                           _svg_w5t6nb,
                           allowDrawingOutsideViewBox: true,
                           fit: BoxFit.fill,
-                        )),
+                        ),
                       ),
                     ],
                   ),
@@ -164,79 +124,42 @@ class signUp extends StatelessWidget {
             ),
           ),
           Pinned.fromPins(
-            Pin(size: 120.0, end: 46.0),
-            Pin(size: 45.0, end: 33.0),
-            child: Stack(
-              children: <Widget>[
-                Pinned.fromPins(
-                  Pin(size: 76.0, end: 44.0),
-                  Pin(size: 45.0, end: 0.0),
-                  child: Stack(
-                    children: <Widget>[
-                      SizedBox.expand(
-                          child: Text(
-                        '다음 ',
-                        style: TextStyle(
-                          fontFamily: 'Apple SD Gothic Neo',
-                          fontSize: 38,
-                          color: const Color(0xff00878d),
-                          fontWeight: FontWeight.w800,
-                        ),
-                        softWrap: false,
-                      )),
-                    ],
-                  ),
-                ),
-                Pinned.fromPins(
-                  Pin(size: 44.0, end: 0.0),
-                  Pin(start: 0.0, end: 1.0),
-                  child: iconnext(),
-                ),
-              ],
+            Pin(size: 27.0, start: 19.0),
+            Pin(size: 27.0, start: 37.0),
+            child: iconclose(),
+          ),
+          Pinned.fromPins(
+            Pin(startFraction: 0.0833, endFraction: 0.0833),
+            Pin(startFraction: 0.825, endFraction: 0.0484),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff00878d),
+                borderRadius: BorderRadius.circular(41.0),
+              ),
             ),
           ),
           Pinned.fromPins(
-            Pin(start: 37.0, end: 36.0),
-            Pin(startFraction: 0.7219, endFraction: 0.1875),
+            Pin(startFraction: 0.3472, endFraction: 0.3472),
+            Pin(startFraction: 0.85, endFraction: 0.0734),
             child: Stack(
               children: <Widget>[
                 Pinned.fromPins(
-                  Pin(startFraction: 0.0, endFraction: 0.6202),
-                  Pin(startFraction: 0.0, endFraction: 0.5862),
+                  Pin(startFraction: 0.0, endFraction: 0.0),
+                  Pin(startFraction: 0.0, endFraction: 0.0),
                   child: Text(
-                    '비밀번호 확인',
+                    '로그인',
                     style: TextStyle(
                       fontFamily: 'Apple SD Gothic Neo',
-                      fontSize: 20,
-                      color: const Color(0xff1d1d1d),
-                      fontWeight: FontWeight.w600,
+                      fontSize: 41,
+                      color: const Color(0xffffffff),
+                      letterSpacing: 1.845,
+                      fontWeight: FontWeight.w700,
                     ),
                     softWrap: false,
                   ),
                 ),
-                Pinned.fromPins(
-                  Pin(start: 0.0, end: 0.0),
-                  Pin(size: 0.0, end: 0.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, -1.0),
-                        child: SizedBox.expand(
-                            child: SvgPicture.string(
-                          _svg_w5t6nb,
-                          allowDrawingOutsideViewBox: true,
-                          fit: BoxFit.fill,
-                        )),
-                      ),
-                    ],
-                  ),
-                ),
               ],
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(19.0, 37.0, 314.0, 576.0),
-            child: iconclose(),
           ),
         ],
       ),
